bc2b4e3fba5992cf4d9bbe5844e0522b407a30c0
Jonathan Abreu <jonathan.pereira@prill.com.br>
2022-08-17 07:14:07 -0300

    Api gateway created

---
+++ hr-api-gateway/src/test/java/com/jonathan/hrapigateway/HrApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.hrapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
