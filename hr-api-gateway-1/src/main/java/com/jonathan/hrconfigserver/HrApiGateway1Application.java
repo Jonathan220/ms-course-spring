dd8185dc233a94709decbc97b8ce5275f23230e1
Jonathan Abreu <jonathan.pereira@prill.com.br>
2022-08-22 08:02:40 -0300

    hr-config-server created

---
+++ hr-api-gateway-1/src/main/java/com/jonathan/hrconfigserver/HrApiGateway1Application.java
@@ -0,0 +1,13 @@
+package com.jonathan.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGateway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGateway1Application.class, args);
+	}
+
+}
---
+++ hr-api-gateway-1/src/test/java/com/jonathan/hrconfigserver/HrApiGateway1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGateway1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
