76f8b285634ad8482e288f60a7e376065a10ab36
Jonathan Abreu <jonathan.pereira@prill.com.br>
2022-08-27 12:03:08 -0300

    Configuring hr-config-server project

--- hr-worker/src/main/java/com/jonathan/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/jonathan/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,9 @@ public class WorkerResource {
 
 	private static Logger log = LoggerFactory.getLogger(WorkerResource.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private WorkerRepository repository;
 	
@@ -32,6 +36,13 @@ public class WorkerResource {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
 	}
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs(){
+		log.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
