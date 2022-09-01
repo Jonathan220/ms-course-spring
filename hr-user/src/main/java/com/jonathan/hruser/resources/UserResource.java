89d32a6fd9f5ee6047c8bea055e23b3ad560d1df
Jonathan Abreu <jonathan.pereira@prill.com.br>
2022-09-01 09:28:48 -0300

    UserRepository, UserResource e Api Gateway config

---
+++ hr-user/src/main/java/com/jonathan/hruser/resources/UserResource.java
@@ -0,0 +1,34 @@
+package com.jonathan.hruser.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jonathan.hruser.entities.User;
+import com.jonathan.hruser.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User result = repository.findById(id).get();
+		return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping(value = "/search")
+	public ResponseEntity<User> findById(@RequestParam String email){
+		
+		User result = repository.findByEmail(email);
+		return ResponseEntity.ok(result);
+	}
+}
