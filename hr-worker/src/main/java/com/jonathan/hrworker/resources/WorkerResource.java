646a5489e13cc9fabcc26b4641f4577b25844c9e
Jonathan Abreu <jonathan.pereira@prill.com.br>
2022-09-08 08:18:59 -0300

    db config server

--- hr-worker/src/main/java/com/jonathan/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/jonathan/hrworker/resources/WorkerResource.java
@@ -49,11 +49,11 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
-		try {
-			Thread.sleep(3000L);
-		}catch(InterruptedException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			Thread.sleep(3000L);
+//		}catch(InterruptedException e) {
+//			e.printStackTrace();
+//		}
 		
 		log.info("PORT = "+ env.getProperty("local.server.port"));
 		
